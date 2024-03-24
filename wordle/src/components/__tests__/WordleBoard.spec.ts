3e1f67a76608e8aa791c9992ffa631aca1f60609
Kate Kim <kay.k.projects@gmail.com>
2024-03-24 16:11:56 +1000

    remove clutter

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -3,15 +3,12 @@ import WordleBoard from '../WordleBoard.vue'
 
 describe('WordleBoard', () => {
  test("a victory message", async() => {
-  // arrange
   const wrapper = mount(WordleBoard, { props: { wordOfTheDay: "TESTS" }})
 
-  // act
   const guessInput = wrapper.find("input[type=text]")
   await guessInput.setValue("TESTS")
   await guessInput.trigger("keydown.enter")
 
-  // assert
   expect(wrapper.text()).toContain("You Won!")
  })
 })

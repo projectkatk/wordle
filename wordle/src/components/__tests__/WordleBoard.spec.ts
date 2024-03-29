86e3c0e1b10601c9721cd0faf9b7da7736992aec
Kate Kim <kay.k.projects@gmail.com>
2024-03-29 23:50:37 +1000

    more

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -60,6 +60,19 @@ describe('WordleBoard', () => {
   })
 
   describe("Player input", () => {
+
+    test("remains in focus the entire time", async () => {
+      document.body.innerHTML = `<div id="app"></div>`
+      wrapper = mount(WordleBoard, {
+        props: {wordOfTheDay},
+        attachTo: "#app"
+      })
+
+      expect(wrapper.find("input[type=text]").attributes("autofocus")).not.toBeUndefined()
+
+      await wrapper.find("input[type=text]").trigger("blur")
+      expect(document.activeElement).toBe(wrapper.find("input[type=text]").element)
+    })
     test(`player guesses are limited to ${WORD_SIZE} letters`, async () => {
       await playerSubmitsGuess(wordOfTheDay + 'EXTRA')
       
@@ -92,4 +105,5 @@ describe('WordleBoard', () => {
       expect(wrapper.find<HTMLInputElement>('input[type=text]').element.value).toEqual("")
     })
   })
+
 })
